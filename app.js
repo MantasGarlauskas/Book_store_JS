60320beb5b5b52d142de4c4259d15f194236e01a
MantasGarlauskas <mantas.garlauskas@gmail.com>
2022-01-07 21:11:34 +0200

    search bar update

--- app.js
+++ app.js
@@ -42,38 +42,3 @@ fetch('https://in3.dev/knygos/')
         data = strukura;
         render();
     });
-
-const searchInput = document.querySelector('input#search_input');
-
-searchInput.addEventListener("input", (e) => {
-    let value = e.target.value
-
-    if (value && value.trim().length > 0) {
-        value = value.trim().toLowerCase()
-
-        //returning only the results of setList if the value of the search is included in the person's name
-        setList(data.filter(person => {
-            return person.author.includes(value)
-        }))
-    }
-});
-
-function setList(results) {
-
-    for (const person of results) {
-        // creating a li element for each result item
-        const resultItem = document.createElement('li')
-
-        // // adding a class to each item of the results
-        // resultItem.classList.add('result-item')
-
-        // grabbing the name of the current point of the loop and adding the name as the list item's text
-        const text = document.createTextNode(data.author)
-
-        // appending the text to the result item
-        resultItem.appendChild(text)
-
-        // appending the result item to the list
-        container.appendChild(resultItem)
-    }
-}
